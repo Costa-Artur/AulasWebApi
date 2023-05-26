87eea3383c2c10510e6fab9e7ba6bc49fe22cd0a
Artur Costa <souza.artur@edu.univali.br>
2023-05-26 17:50:58 -0300

    nova função post

--- Univali/src/Univali.Api/Controllers/CustomersController.cs
+++ Univali/src/Univali.Api/Controllers/CustomersController.cs
@@ -14,13 +14,43 @@ public class CustomersController : ControllerBase
         return Ok(result);
     }
 
-    [HttpGet("{id}")]
-    public ActionResult<Customer> GetCustomer ([FromRoute] int id) 
+    [HttpGet("{id}", Name = "GetCustomerById")]
+    public ActionResult<Customer> GetCustomerById (int id) 
     {
+        Console.WriteLine($"id: {id}");
         var result = Data.Instance.Customers.FirstOrDefault(c => c.Id == id);
 
         if(result != null) return Ok(result);
 
         return NotFound();
+    }
+
+    [HttpGet("cpf/{cpf}")]
+    public ActionResult<Customer> GetCustomerByCpf (string cpf) 
+    {
+        Console.WriteLine($"cpf: {cpf}");
+        var result = Data.Instance.Customers.FirstOrDefault(c => c.Cpf == cpf);
+
+        if(result != null) return Ok(result);
+
+        return NotFound();
+    }
+    [HttpPost]
+    public ActionResult<Customer> CreateCustomer (Customer customer) 
+    {
+        var newCustomer = new Customer 
+        {
+            Id = Data.Instance.Customers.Max(c => c.Id)+1,
+            Name = customer.Name,
+            Cpf = customer.Cpf
+        };
+
+        Data.Instance.Customers.Add(newCustomer);
+        return CreatedAtRoute
+        (
+            "GetCustomerById",
+            new {id = newCustomer.Id },
+            newCustomer
+        );
     }
 }
