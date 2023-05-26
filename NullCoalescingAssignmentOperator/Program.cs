0fcafdcbf0089882fa61d329c04a5f354f9a65a0
Artur Costa <souza.artur@edu.univali.br>
2023-05-26 14:55:22 -0300

    esse nome ai

---
+++ NullCoalescingAssignmentOperator/Program.cs
@@ -0,0 +1,19 @@
+﻿string? name1 = null;
+string? name2 = null;
+string? name3 = "Richard Stallman";
+string defaultName = "Mark Zuckerberg";
+
+if(name1 == null) 
+{
+    name1 = defaultName;
+}
+
+Console.WriteLine($"name1: {name1}");
+
+name2 ??= defaultName;
+
+Console.WriteLine($"name2: {name2}");
+
+name3 ??= defaultName;
+
+Console.WriteLine($"name3: {name3}");
