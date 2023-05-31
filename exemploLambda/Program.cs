afea1613041c258694aa4e05c18f3694a5709ca9
Artur Costa <souza.artur@edu.univali.br>
2023-05-31 14:44:46 -0300

    Exemplo funcao lambda

---
+++ exemploLambda/Program.cs
@@ -0,0 +1,9 @@
+﻿Func<int, int> TripleExpression = x => x*3;
+
+Func<int, int> TripleStatement = number =>
+{
+    var result = number * 3;
+    return result;
+};
+
+Console.WriteLine($"3 x 5 = {TripleStatement(5)}");
